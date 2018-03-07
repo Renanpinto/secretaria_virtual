ac5dfff2605e90a136e1f4cb552b58e66f52eab4
Renan de Souza Pinto <renan.de.souza.pinto@avanade.com>
2018-03-06 23:00:47 -0300

    initial commit

---
+++ webpack/plugins/htmlPlugin.js
@@ -0,0 +1,36 @@
+const
+  path              = require('path'),
+  manifest          = require('../manifest'),
+  HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const titles = {
+  'index': 'Dashboard',
+  'blank': 'Blank',
+  'calendar': 'Calendar',
+  'charts': 'Charts',
+  'chat': 'Chat',
+  'compose': 'Compose',
+  'email': 'Email',
+  'forms': 'Forms',
+  'signin': 'Signin',
+  'signup': 'Signup',
+  '404': '404',
+  '500': '500',
+  'basic-table': 'Basic Table',
+};
+
+module.exports = Object.keys(titles).map(title => {
+  return new HtmlWebpackPlugin({
+    template: path.join(manifest.paths.src, `${title}.html`),
+    path: manifest.paths.build,
+    filename: `${title}.html`,
+    inject: true,
+    minify: {
+      collapseWhitespace: true,
+      minifyCSS: true,
+      minifyJS: true,
+      removeComments: true,
+      useShortDoctype: true,
+    },
+  });
+});
